a279ad0e2507dd7f5cba09e80f8d9fba3440e98d
HanumGoo <sheehanandya001@gmail.com>
2026-07-21 15:11:57 +0700

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = new bootstrap.Modal(document.getElementById("imageModal"));
+const modalImage = document.getElementById("modalImage");
+
+document.querySelectorAll(".gallery-img").forEach((img) => {
+  img.addEventListener("click", () => {
+    modalImage.src = img.src;
+    modal.show();
+  });
+});
